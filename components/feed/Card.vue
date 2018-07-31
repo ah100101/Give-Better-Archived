93be40c0f0722d35fb15492cb649add2d62fafb5
ah100101 <ah100101@gmail.com>
2018-07-30 22:25:58 -0500

    renamed badge and changed script

--- components/feed/Card.vue
+++ components/feed/Card.vue
@@ -28,7 +28,7 @@
 </template>  
 
 <script>
-import badge from '~/components/feed/badge.vue'
+import badge from '~/components/feed/Badge.vue'
 import basket from '~/components/feed/Basket.vue'
 
 export default {
